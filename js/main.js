5b42a3c0f8248d69ab0865879a0fa59ab7abcf55
Oleg Chugulev <chugulevoleg@gmail.com>
2023-09-19 21:44:56 +0700

    Dick

--- js/main.js
+++ js/main.js
@@ -2,9 +2,35 @@ let offset = 0;
 let dotIndex = 0;
 const swiperWrapper = document.querySelector('.swiper__wrapper');
 const dots = document.querySelectorAll('.slide__dot');
+const dotsTwo = document.querySelectorAll('.slide__dot-two')
+const swiperWrapperTwo = document.querySelector('.two-wrapper');
 
-
-
+document.querySelector('.two-next').addEventListener('click', function(){
+    offset += 270;
+    dotIndex++
+    if(offset > 810){
+        offset = 0;
+        dotIndex = 0
+    }
+    swiperWrapperTwo.style.left = -offset + "px";
+    thisSlideTwo(dotIndex);
+})
+document.querySelector('.two-prev').addEventListener('click', function(){
+    offset -= 270;
+    dotIndex--
+    if(offset < 0){
+        offset = 810;
+        dotIndex = (dots.length - 1)
+    }
+    swiperWrapperTwo.style.left = -offset + "px";
+    thisSlideTwo(dotIndex);
+})
+function thisSlideTwo(index){
+    for(let dot of dotsTwo){
+        dot.classList.remove('active__dot-two')
+    }
+    dotsTwo[index].classList.add('active__dot-two')
+}
 
 document.querySelector('.swiper__next').addEventListener('click', function(){
     offset += 270;
